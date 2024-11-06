83024f2fea9644ff691293650c5f78691e9bd016
sharmachait <chait8126@gmail.com>
2024-11-07 00:14:20 +0530

    attrocities

---
+++ backend/wazir/src/main/java/com/sharmachait/wazir/WazirApplication.java
@@ -0,0 +1,13 @@
+package com.sharmachait.wazir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazirApplication.class, args);
+	}
+
+}
